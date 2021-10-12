a4247bbfc8c17f39865a8f9c4d3f503ca410598a
SVRourke <samuelvanrourke@gmail.com>
2021-10-12 12:32:50 -0400

    fixed ignored argument

--- lib/correlation.py
+++ lib/correlation.py
@@ -1,11 +1,12 @@
 def crosscorr(data_a, data_b, lag=0):
     return data_a.corr(data_b.shift(lag))
 
+
 def get_correlations(leader, follower, n=10):
     measurements = []
 
-    for i in range(0, 50):
+    for i in range(1, n):
         corr_measure = crosscorr(leader, follower, i)
         measurements.append(corr_measure)
-    
+
     return measurements
